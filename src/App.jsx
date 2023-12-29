ec06e1598e57c1c7aaef75d605c6f1306289526b
J-Maica <jmtdelremedio@gmail.com>
2023-12-30 04:01:19 +0800

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import Navbar from './component/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Footer from './component/Footer'
+import ReactGA from 'react-ga'
 import './App.css'
 import "react-lazy-load-image-component/src/effects/blur.css";
 
+const TRACKING = import.meta.env.VITE_TRACKING_ID
+ReactGA.initialize(TRACKING)
 
 export const AppContext = createContext();
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
 
   // navbar toggle
   const [toggle, setToggle] = useState(false)
